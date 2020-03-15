f289d244ee6d67332a0239244442c78c61fde9e3
paulwoojungkim <wj2kim@gmail.com>
2020-03-15 18:02:12 +0900

    builder pattern commit

--- src/part2_BuilderPattern/NutritionFacts.java
+++ src/part2_BuilderPattern/NutritionFacts.java
@@ -1,5 +1,49 @@
 package part2_BuilderPattern;
 
 public class NutritionFacts {
-
+	private final int servingSize;
+	private final int servings;
+	private final int calories;
+	private final int fat;
+	private final int sodium;
+	private final int carbohydrate;
+	
+	
+	public static class Builder {
+		// 필수 인자 
+		private final int servingSize;
+		private final int servings;
+		// 선택적 인자 - 기본값으로 초기화 
+		private int calories = 0;
+		private int fat = 0;
+		private int carbohydrate = 0;
+		private int sodium = 0;
+		
+		public Builder (int servingSize, int servings) {
+			this.servingSize = servingSize;
+			this.servings = servings;
+		}
+		
+		public Builder calories (int val)
+		{ calories = val ; return this; }
+		public Builder fat (int val)
+		{ fat = val ; return this; }
+		public Builder carbohydrate(int val)
+		{ carbohydrate = val; return this; }
+		public Builder sodium (int val)
+		{ sodium = val; return this; }
+		
+		public NutritionFacts build() {
+			return new NutritionFacts(this);
+		}
+	}
+	
+	private NutritionFacts(Builder builder) {
+		servingSize = builder.servingSize;
+		servings = builder.servings;
+		calories = builder.calories;
+		fat = builder.fat;
+		sodium = builder.sodium;
+		carbohydrate = builder.carbohydrate; 
+	}
 }
